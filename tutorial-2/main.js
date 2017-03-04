5621acbbf7523b486cd3f7ad1b4485081e013357
Pand-a <dami.thibault@gmail.com>
2017-03-04 15:27:53 -0500

    Revert "ajouter un commentaire"
    This reverts commit 02a867e4d62d3acff46cf35fa882f04bef104b3a.

--- tutorial-2/main.js
+++ tutorial-2/main.js
@@ -1,7 +1,7 @@
 var roleHarvester = require('role.harvester');
 
 module.exports.loop = function () {
-	//Ceci est un commentaire ! ok la la !
+
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
         roleHarvester.run(creep);
