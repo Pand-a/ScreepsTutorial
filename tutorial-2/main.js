438fba7f95b414c79f035738e4d06e0593165dea
Pand-a <dami.thibault@gmail.com>
2017-03-04 16:19:00 -0500

    End of tutorial 2

--- tutorial-2/main.js
+++ tutorial-2/main.js
@@ -1,9 +1,17 @@
 var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
 
 module.exports.loop = function () {
-
+    //console.log("Hello my memory now is : ")
+	//Ceci est un commentaire ! ok la la !
     for(var name in Game.creeps) {
+        //console.log("the name is now at :" + name)
         var creep = Game.creeps[name];
-        roleHarvester.run(creep);
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
     }
 }
